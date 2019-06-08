053a8e5a9cb53894bb247802bd43451840b69a96
Maria Lind <daniela.lind@globant.com>
2019-06-08 18:32:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleSimplex from 'simple-simplex'
 
 function App() {
+  const solver = new SimpleSimplex({
+    objective: {
+      a: 70,
+      b: 210,
+      c: 140,
+    },
+    constraints: [
+      {
+        namedVector: { a: 1, b: 1, c: 1 },
+        constraint: '<=',
+        constant: 100,
+      },
+      {
+        namedVector: { a: 5, b: 4, c: 4 },
+        constraint: '<=',
+        constant: 480,
+      },
+      {
+        namedVector: { a: 40, b: 20, c: 30 },
+        constraint: '<=',
+        constant: 3200,
+      },
+    ],
+    optimizationType: 'max',
+  });
+   
+  // call the solve method with a method name
+  const result = solver.solve({
+    methodName: 'simplex',
+  });
+   
+  // // see the solution and meta data
+  // console.log({
+  //   solution: result.solution,
+  //   isOptimal: result.details.isOptimal,
+  // });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => {console.log(result)}}>holis</button>
+        <div>
+          {'isOptimal ' + result.details.isOptimal}
+        </div>
     </div>
   );
 }
